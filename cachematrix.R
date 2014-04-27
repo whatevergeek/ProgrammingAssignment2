e65936675e7add83349b0b32dce9aa57ea519411
Paul <whatevergeek@gmail.com>
2014-04-28 05:28:14 +0800

    Modified cachematrix.R: Added implementation for cacheSolve
    Added implementation for cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -21,5 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverseMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverseMatrix(m)
+  m
 }
